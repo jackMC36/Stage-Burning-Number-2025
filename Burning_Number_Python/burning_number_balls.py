bf1da0a9fd950afeb4f1ca42efca8ce24c5ac4b9
Jacques KOZIK <Jacques.KOZIK@etu.uca.fr>
2025-06-17 15:28:29 +0200

    COMMIT

--- Burning_Number_Python/burning_number_balls.py
+++ Burning_Number_Python/burning_number_balls.py
@@ -8,6 +8,7 @@ import copy
 ##################################################################################################################################################################
 ##################################################################################################################################################################
 ##################################################################################################################################################################
+
 cpt = 0
 class Graph:
     '''Un graph G=(V,E)
@@ -30,13 +31,24 @@ class Graph:
     def get_edges(self) -> list[Tuple[int,int]]:
         return self.E
     
-    def get_neigbhors(self,s:int) -> List[int]:
-        l = []
+    def get_neigbhors(self,v:int) -> set[int]:
+        l = set()
         for e in self.E:
-            if s == e[0]:
-                l.append(e[1])
-            if s == e[1]:
-                l.append(e[0])
+            if v == e[0]:
+                l.add(e[1])
+            if v == e[1]:
+                l.add(e[0])
+        return l
+    
+    def get_neighbors_distant(self, v: int, distance: int) -> set[int]:
+        l = set()
+        l.add(v)
+        if distance<=0:
+            return l
+        temp = self.get_neigbhors(v)
+        l.update(temp)
+        for vertice in temp:
+            l.update(self.get_neighbors_distant(vertice,distance-1))
         return l
     
     def txt_file_to_graph(self, fd: str) -> None:
@@ -73,16 +85,6 @@ class Graph:
         nx.draw(self.G_nx, pos, with_labels=True, node_color='orange', edge_color='gray')
         plt.show()
     
-    def score(self, sommet: int, n: int):
-        '''Correspond au score obtenu en plaçant une balle de taille degre sur le sommet en paramètre.'''
-        if n == 0:
-            return len(self.get_neigbhors(sommet))
-        else:
-            N = self.get_neigbhors(sommet)
-            somme = 0
-            for v in N:
-                somme += self.score(v,n-1)
-            return somme/len(N)
         
     def diametre(self):
         '''Calcule le diametre du Graph.'''
@@ -96,10 +98,7 @@ class Graph:
         
     def borne(self):
         '''Renvoie la borne supérieure du burning number.'''
-        return max(math.ceil(len(self.get_vertices())**0.5),self.diametre())
-
-
-
+        return min(math.ceil(len(self.get_vertices())**0.5),self.diametre())
 
 ##################################################################################################################################################################
 ##################################################################################################################################################################
@@ -116,12 +115,12 @@ class Etat:
     B_v la liste de sommets v brulés par la balle de taille B.
     C la liste du sommet au centre de chaque balle.
     '''
-    def __init__(self, NB: set[int], B: set[int], E: List[Tuple[int, int]], n: int, B_v : List[List[int]], C : List[int]):
+    def __init__(self, NB: set[int], B: set[int], G: Graph, n: int, B_v : List[List[int]], C : List[int]):
         '''Le constructeur spécifie une liste de sommets non-brulés NB, une liste de sommets brulés B,  et une liste de arêtes E, un entier n correspondant à la taille de la balle maximale, et B_v une liste de liste de taille |V|*B correspondant à la liste des sommets brulés par la balle de taille de 1..B.'''
         global cpt
         self.B = set(B)
         self.NB = set(NB)
-        self.E = set(E)
+        self.G = G
         self.n = n
         self.B_v = B_v
         self.C = C
@@ -131,12 +130,18 @@ class Etat:
 
         score = 0
         for i in range(len(B_v)):
-            score+=len(B_v[i])/((i+1)**2)
+            score+=self.score_sommet(self.C[i],i)
 
-        self.score = score
+        self.score = score - self.diametre_NB()**2
+
+    def score_sommet(self, sommet: int, n: int):
+        '''Correspond au score obtenu en plaçant une balle de taille n sur le sommet en paramètre.'''
+        if n == 0:
+            return len(self.G.get_neigbhors(sommet))
+        return len(self.G.get_neighbors_distant(sommet,n))//n**2
 
     def __str__(self):
-        return "------------------------------\n"+"Identifiant de Etat: "+cpt.__str__() +"\n"+"Liste des sommets brulés: " + self.B.__str__() + "\n"+ "sommets non brulés" + self.NB.__str__() + "\n" + "Listes de balles: \n" + self.B_v.__str__() +"\n"+ "score: "+ self.score.__str__() + "\n" + "Sommets centres de balles: \n" + self.C.__str__() + "\n" +"------------------------------\n"
+        return "------------------------------\n"+"Identifiant de Etat: "+cpt.__str__() +"\n"+"Liste des sommets brulés: " + self.B.__str__() + "\n"+ "sommets non brulés" + self.NB.__str__() + "\n" + "Listes de balles: \n" + self.B_v.__str__() +"\n"+ "score: "+ self.score.__str__() + "\n" + "Sommets centres de balles: \n" + self.C.__str__() + "\n" +"b(G):"+ len(self.C).__str__() + "\n" +"------------------------------\n"
 
     def get_B_v(self) -> List[List[int]]:
         '''Retourne la liste de sommets v brulés par la balle de taille 1..B'''
@@ -145,6 +150,15 @@ class Etat:
     def get_B(self) -> set[int]:
         '''Retourne la sequence de sommets brulés.'''
         return self.B
+    
+    def diametre_NB(self):
+        '''Calcule le diametre du Graph.'''
+        len_max = 0
+        for i in self.NB:
+            for j in self.NB:
+                if nx.shortest_path_length(self.G.G_nx, i, j) > len_max:
+                    len_max = nx.shortest_path_length(self.G.G_nx, i, j)
+        return len_max
 
     def get_NB(self) -> set[int]:
         '''Retourne la sequence de sommets non-brulés.'''
@@ -159,7 +173,6 @@ class Etat:
         return self.score
     
     def get_C(self) -> List[int]:
-
         return self.C
 
 ##################################################################################################################################################################
@@ -172,11 +185,11 @@ class Burning_Number:
     def __init__(self, G: Graph):
         '''Le constructeur specifie le graph G=(V,E).'''
         self.G = G
-        self.Etat = Etat(self.G.get_vertices(),[],self.G.get_edges(),0,[],[])
+        self.Etat = Etat(self.G.get_vertices(),[],self.G,0,[],[])
 
     def etat_initial(self) -> Etat:
         '''Retourne l'état initial'''
-        return Etat(self.G.get_vertices(),[],self.G.get_edges(),0,[],[])
+        return Etat(self.G.get_vertices(),[],self.G,0,[],[])
     
     def actions(self, etat: Etat) -> set[int]:
         '''Retourne les actions qui peuvent etre prise à partir d'un état donner.'''
@@ -204,12 +217,11 @@ class Burning_Number:
                             B_vi_copy.append(l)
             new_B_v[i] = B_vi_copy
 
-        etat_intermediaire = Etat(new_NB,new_B,self.G.get_edges(),new_n,new_B_v,new_C)
+        etat_intermediaire = Etat(new_NB,new_B,self.G,new_n,new_B_v,new_C)
         return etat_intermediaire
     
     def succ(self, etat: Etat, action: int) -> Etat:
         '''Retourne l'etat obtenu après avoir executé une action sur un état.'''
-
 
         new_NB = etat.get_NB().copy()
         new_B = etat.get_B().copy()
@@ -224,25 +236,25 @@ class Burning_Number:
         new_C.insert(0,action)
         new_B.add(action)
         new_NB.discard(action)
-        new_etat = Etat(new_NB,new_B,self.G.get_edges(),new_n, new_B_v,new_C)
+        new_etat = Etat(new_NB,new_B,self.G,new_n, new_B_v,new_C)
         return new_etat
         
-        
-        
-        
-
     def goal_test(self, etat: Etat) -> bool:
         '''Verifie si l'etat est un etat but. '''
         return etat.get_NB() == set()
     
     def traiter(self) -> List["Noeud"]:
-        Racine1 = Noeud(self.etat_initial())
-        L_Noeud = [Racine1]
-        L_a_traiter = [Racine1]
-
+        niveau = self.G.borne()//2
+        etat_initial = self.etat_initial()
+        noeud_initial = Noeud(etat_initial,None,None,0,0)
+        L_Noeud = [noeud_initial]
+        L_a_traiter = [noeud_initial]
+        i = -1
         while(L_a_traiter != []):
+            i+=1
             Courant = L_a_traiter[0]
-            L_Enfant = Courant.expand(self)
+            print(Courant.get_Etat().__str__())
+            L_Enfant = Courant.expand(self,niveau-i)
             L_Enfant_Trie = []
             for n in L_Enfant:
                 if L_Enfant_Trie == []:
@@ -255,6 +267,9 @@ class Burning_Number:
             L_a_traiter.remove(Courant)
             L_a_traiter.extend(L_Enfant_Trie[len(L_Enfant_Trie)//2 : len(L_Enfant_Trie)])
             L_Noeud.extend(L_Enfant_Trie[len(L_Enfant_Trie)//2 : len(L_Enfant_Trie)])
+            for l in L_Enfant_Trie:
+                if l.get_Etat().get_NB() == set():
+                    L_a_traiter = []
  
         return L_Noeud
     
@@ -268,15 +283,10 @@ class Burning_Number:
         L_goal_noeud.sort(key=lambda n: n.get_Etat().get_score(), reverse=True)
         return L_goal_noeud
 
-
-    
-
-
-
 ##################################################################################################################################################################
 ##################################################################################################################################################################
 ##################################################################################################################################################################
-########################################################################################################toy_model_2##########################################################
+##################################################################################################################################################################
 
 class Noeud:
     '''Noeud dans un arbre de recherche. Un noeud contient :
@@ -290,6 +300,7 @@ class Noeud:
         self.Etat = Etat
         self.parent = parent
         self.depth = 0
+        self.cout = cout
         self.n = n
         if parent:
             self.depth = parent.depth + 1
@@ -311,24 +322,20 @@ class Noeud:
         for i in L_actions:
             L_Children.append(Burning_Number.succ(self,i))
     
-    def expand(self, problem: Burning_Number) -> List["Noeud"]:
+    def expand(self, problem: Burning_Number, niveau: int) -> List["Noeud"]:
         '''Retoune la liste des Noeuds atteignable en une etape à partir du noeud courant.'''
         L_Noeud = []
         Courant = self
         Etat_Intermediaire = problem.propagation(Courant.get_Etat())
         L_actions = problem.actions(Etat_Intermediaire)
+
         for i in L_actions:
             Etat_enfant = problem.succ(Etat_Intermediaire,i)
             Noeud_enfant = Noeud(Etat_enfant,Courant,i,Courant.get_Cout()+1)
             L_Noeud.append(Noeud_enfant)
-        return L_Noeud
+        L_Noeud.sort(key=lambda n: n.get_Etat().get_score(), reverse=False)
+        return L_Noeud[len(L_Noeud)//2 : len(L_Noeud)]
     
-
-
-
-
-
-
 ##################################################################################################################################################################
 ##################################################################################################################################################################
 ##################################################################################################################################################################
@@ -337,18 +344,22 @@ class Noeud:
 ## Test ##
 
 G1 = Graph([], [])
-G1.txt_file_to_graph("Instances/karate.txt")
+G1.txt_file_to_graph("Instances/ca-netscience.txt")
 B1 = Burning_Number(G1)
 L1 = B1.traiter()
 L2 = B1.noeuds_goal(L1)
+taille_balle_min = len(L2[0].get_Etat().get_C())-1
+
+for l in L2:
+    if len(l.get_Etat().get_C())-1 < taille_balle_min:
+        taille_balle_min = len(l.get_Etat().get_C())-1
+
 print("########################\n")
 print("Il y a un total de " + len(L1).__str__() + " noeuds retenues")
-print("Avec un total de " + len(L2).__str__() + " noeuds goals")
 print("########################\n")
-
-print("\n")
-print("Les meilleur noeud obtenu est:\n")
-for n in L2:
-    print(n.get_Etat().__str__())
-
+print("Le noeud de b(G) min est:\n")
+for l in L2:
+    if len(l.get_Etat().get_C())-1 == taille_balle_min:
+        print(l.get_Etat().__str__())
+print("########################\n")
 G1.show()
